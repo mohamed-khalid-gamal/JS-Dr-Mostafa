6dc35fa2793e0519d3a11a786d9724cc509ac1f9
mohamed-khalid-gamal <mohamed.khalid.gamal@gmail.com>
2025-07-05 20:37:49 +0300

    console

---
+++ Basics/02 Printing/Console/index.js
@@ -0,0 +1,24 @@
+// // Simple text output
+// console.log("Hello, World!");
+
+// // Variable output
+// let name = "Alice";
+// console.log("User name:", name);
+
+// // Multiple values
+// console.log("Age:", 25, "City:", "New York");
+
+// // Object output
+// let user = {name: "Bob", age: 30};
+// console.log("User object:", user);
+
+// console.warn("This is a warning message");
+
+// console.error("An error occurred: Data not found.");
+// console.info("Application started successfully.");
+
+// const users = [
+//     { name: 'John', age: 25 },
+//     { name: 'Jane', age: 30 }
+// ];
+// console.table(users);
